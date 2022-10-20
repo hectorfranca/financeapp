34cb3056ae78ca853d02a953691fa12299915f90
hectorfranca <hectorjuliomartinsdefranca@hotmail.com>
2022-10-20 18:11:58 -0300

    enviando sql do banco de dados

---
+++ sql/financeapp.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 20-Out-2022 às 19:13
+-- Versão do servidor: 5.7.36
+-- versão do PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `financeapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `Id` int(11) NOT NULL,
+  `Nome` varchar(40) COLLATE latin1_bin NOT NULL,
+  `Tipo` char(1) COLLATE latin1_bin NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `registro`
+--
+
+DROP TABLE IF EXISTS `registro`;
+CREATE TABLE IF NOT EXISTS `registro` (
+  `Id` int(11) NOT NULL,
+  `Categoria_id` int(11) NOT NULL,
+  `Nome` varchar(60) COLLATE latin1_bin NOT NULL,
+  `Data` date NOT NULL,
+  `Valor` double NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_categoriaid` (`Categoria_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `registro`
+--
+ALTER TABLE `registro`
+  ADD CONSTRAINT `fk_categoriaid` FOREIGN KEY (`Categoria_id`) REFERENCES `categoria` (`Id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
