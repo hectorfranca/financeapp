d6e682712bb3de4a3172835c33bcb23dec4e0c77
hectorfranca <hectorjuliomartinsdefranca@hotmail.com>
2022-10-24 10:20:10 -0300

    alterando versao da dependencia jakarta para 5.0.0

--- target/generated-sources/annotations/Beans/CategoriaBean_.java
+++ target/generated-sources/annotations/Beans/CategoriaBean_.java
@@ -2,16 +2,17 @@ package Beans;
 
 import Beans.RegistroBean;
 import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-10-21T18:38:46")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-10-24T10:12:39")
 @StaticMetamodel(CategoriaBean.class)
 public class CategoriaBean_ { 
 
     public static volatile SingularAttribute<CategoriaBean, Character> tipo;
+    public static volatile SetAttribute<CategoriaBean, RegistroBean> registros;
     public static volatile SingularAttribute<CategoriaBean, String> nome;
     public static volatile SingularAttribute<CategoriaBean, Long> id;
-    public static volatile SingularAttribute<CategoriaBean, RegistroBean> registro;
 
 }
--- target/generated-sources/annotations/Beans/RegistroBean_.java
+++ target/generated-sources/annotations/Beans/RegistroBean_.java
@@ -3,16 +3,15 @@ package Beans;
 import Beans.CategoriaBean;
 import java.util.Date;
 import javax.annotation.Generated;
-import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-10-21T18:38:46")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-10-24T10:12:39")
 @StaticMetamodel(RegistroBean.class)
 public class RegistroBean_ { 
 
     public static volatile SingularAttribute<RegistroBean, Date> data;
-    public static volatile SetAttribute<RegistroBean, CategoriaBean> categorias;
+    public static volatile SingularAttribute<RegistroBean, CategoriaBean> categoria;
     public static volatile SingularAttribute<RegistroBean, Double> valor;
     public static volatile SingularAttribute<RegistroBean, String> nome;
     public static volatile SingularAttribute<RegistroBean, Long> id;
