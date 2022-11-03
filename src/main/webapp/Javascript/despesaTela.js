8cc691fafe6eb41d1989c474930f0be29a8f40da
luciofreitas <luciodfp@gmail.com>
2022-11-03 12:58:15 -0300

    Despesas.html com problema no seletor

--- src/main/webapp/Javascript/despesaTela.js
+++ src/main/webapp/Javascript/despesaTela.js
@@ -1,2 +1,18 @@
-$('.carousel').carousel({interval: false });
+const seletorDespesas = document.getElementById('seletorDespesas'),
+        listaDespesas = document.getElementById('listaDespesas'),
+        itemDespesas = document.getElementsByClassName('itemDespesas');
+for (let item of itemDespesas) {
+    item.addEventListener("click", (e) => {
+        seletorDespesas.classList.add("show-menu");
+        seletorDespesas.classList.remove("hidden-menu");
+
+        item.addEventListener("mouseout", (e) => {
+            seletorDespesas.classList.add("hidden-menu");
+            seletorDespesas.classList.remove("show-menu");
+        });
+    });
+}
+
+
+
 
--- target/generated-sources/annotations/Beans/RegistroBean_.java
+++ target/generated-sources/annotations/Beans/RegistroBean_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-10-25T22:49:10")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-11-02T12:42:06")
 @StaticMetamodel(RegistroBean.class)
 public class RegistroBean_ { 
 
